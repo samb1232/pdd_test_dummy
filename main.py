49c9829883cb2df71f3dca9cc6b865d2c4c509b7
samb1232 <arterm212@gmail.com>
2026-03-09 22:23:59 +0500

    add webhook listener

--- main.py
+++
@@ -1,7 +0,0 @@
-# @todo #1:30min This is a test puzzle
-#  to see if the local 0pdd bot picks it up.
-# 
-# 
-# # @todo #1:30min Other puzzle123. I hope this will work now.
-# 
-print("Hello World!")
---
+++ webhook_listener.rb
@@ -0,0 +1,50 @@
+require 'webrick'
+require 'json'
+
+server = WEBrick::HTTPServer.new(Port: 9292)
+
+#
+# @todo #1: This is a test puzzle. 123123
+# 
+server.mount_proc '/' do |req, res|
+  if req.request_method == 'POST'
+    puts "\n#{'=' * 50}"
+    puts "🔔 Webhook Received at #{Time.now}"
+    puts "#{'=' * 50}"
+    
+    event_type = req.header['x-github-event']&.first
+    puts "Event Type: #{event_type || 'Unknown'}"
+    
+    puts "\nHeaders:"
+    req.header.each do |key, value|
+      puts "  #{key}: #{value.join(', ')}"
+    end
+
+    puts "\nPayload:"
+    begin
+      if req.body && !req.body.empty?
+        # Attempt to parse and pretty-print JSON payload
+        payload = JSON.parse(req.body)
+        puts JSON.pretty_generate(payload)
+      else
+        puts "  (Empty Body)"
+      end
+    rescue JSON::ParserError
+      # Fallback if it's not valid JSON
+      puts req.body
+    end
+    
+    puts "#{'=' * 50}\n"
+    
+    res.status = 200
+    res.body = "Webhook received successfully"
+  else
+    res.status = 200
+    res.body = "Server is running. Send a POST request to test the webhook."
+  end
+end
+
+trap('INT') { server.shutdown }
+
+puts "🚀 Starting GitHub Webhook listener on http://localhost:9292"
+server.start
