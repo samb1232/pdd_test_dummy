860d6ede1ff54019d7f1485b628f918cb39911c0
samb1232 <arterm212@gmail.com>
2026-03-14 20:23:17 +0500

    delete puzzle

--- webhook_listener.rb
+++ webhook_listener.rb
@@ -3,9 +3,6 @@ require 'json'
 
 server = WEBrick::HTTPServer.new(Port: 9292)
 
-#
-# @todo #2: pls work. TEST 123
-# 
 server.mount_proc '/' do |req, res|
   if req.request_method == 'POST'
     puts "\n#{'=' * 50}"
