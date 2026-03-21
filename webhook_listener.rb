89a9dfef8098262b020e1a3cf977648266a49a6e
samb1232 <arterm212@gmail.com>
2026-03-21 21:32:58 +0500

    meaw

--- webhook_listener.rb
+++ webhook_listener.rb
@@ -15,6 +15,9 @@ server = WEBrick::HTTPServer.new(Port: 9292)
 #
 # @todo #4: Another try
 # 
+#
+# @todo #5: And another one
+# 
 server.mount_proc '/' do |req, res|
   if req.request_method == 'POST'
     puts "\n#{'=' * 50}"
